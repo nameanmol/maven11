7a99a14c1be28c44c6ab9f22e5c6680acc2c4706
nameanmol <38471470+nameanmol@users.noreply.github.com>
2019-10-14 17:29:08 +0530

    Add files via upload

---
+++ src/main/java/maven_00/maven_009/App.java
@@ -0,0 +1,13 @@
+package maven_00.maven_009;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
